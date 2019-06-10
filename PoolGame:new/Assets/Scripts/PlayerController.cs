dac964dbba88da2071637985fd86285af75eb487
KaiChun Yang <kelly21225@gmail.com>
2019-06-10 14:24:22 +0800

    push files

---
+++ PoolGame:new/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public static float speed;
+    public static float offset;
+    private Rigidbody rb;
+
+    public static int count;
+    //public Text Score;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        speed = 50;
+        offset = 1f;
+        count = 0;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            this.gameObject.transform.position = new Vector3(0,0,0);
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+    public void setScore()
+    {
+        //Score.text = "Score: " + Score.ToString();
+    }
+}
---
+++ PoolGame:new/Assets/Scripts/Rotator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rotator : MonoBehaviour {
+    public Text Score;
+    public Text Win;
+
+    void Start()
+    {
+        setScore();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("balls"))
+        {
+            other.gameObject.SetActive(false);
+            PlayerController.count++;
+            setScore();
+        }
+    }
+
+    public void setScore()
+    {
+        Score.text = "Score: " + PlayerController.count.ToString();
+        if (PlayerController.count >= 8)
+        {
+            Win.text = "You Win!";
+        }
+    }
+
+
+}
